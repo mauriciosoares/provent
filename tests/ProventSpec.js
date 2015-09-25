53eb38b0365e591e6e08c5a37e34f2eaec362c08
Mauricio Soares <m.soares2603@gmail.com>
2015-09-25 16:00:52 -0300

    changed testsuite to karma + jasmine

--- tests/ProventSpec.js
+++ tests/ProventSpec.js
@@ -1,5 +1,13 @@
 describe('test', function() {
-  it('shoud do it', function() {
+  beforeEach(function() {
+    // console.log(document);
+  });
+
+  it('should trigger the `then` once the button is clicked', function() {
     expect(true).toBe(true);
+  });
+
+  it('huebrbr', function() {
+    expect(true).toBe(false);
   });
 });
