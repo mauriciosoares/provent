573e0e459fa5402add23eb18e6d7c3372ef8621b
Mauricio Soares <m.soares2603@gmail.com>
2015-09-25 09:17:35 -0300

    add some explanation for a bugfix in testling

--- tests/provent.js
+++ tests/provent.js
@@ -1,3 +1,6 @@
+// in case of "no headless browser found" error, make sure you've installed
+// phantomjs, and if it persists, run this command and everything might start
+// working again: `rm -rf ~/.config/browser-launcher`
 var test = require('tape');
 
 test('beep book', function(t) {
