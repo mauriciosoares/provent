3cf96f028f14d673225a3937707d41395b58987c
Mauricio Soares <m.soares2603@gmail.com>
2015-09-25 09:45:23 -0300

    change tape for prova (which internally uses tape)

--- tests/provent.js
+++ tests/provent.js
@@ -1,7 +1,7 @@
 // in case of "no headless browser found" error, make sure you've installed
 // phantomjs, and if it persists, run this command and everything might start
 // working again: `rm -rf ~/.config/browser-launcher`
-var test = require('tape');
+var test = require('prova');
 var setup = require('./helpers/setup');
 var tearDown = require('./helpers/tearDown');
 
@@ -13,6 +13,8 @@ test('provent::expect `then` callback to be called', function(t) {
   tearDown();
 
   console.log(document.getElementById('test'));
+
+  t.assert(true);
 
   t.end();
 
